047ae57a21fe2b7e695399d0464993ccf5565c8b
tsm <tomszom@gmail.com>
2011-10-28 22:21:31 +0200

    NETWORK_PROVIDER

--- src/bullteam/droidobserver/DroidObserverActivity.java
+++ src/bullteam/droidobserver/DroidObserverActivity.java
@@ -1,9 +1,16 @@
 package bullteam.droidobserver;
 
+import java.util.List;
+
 import android.app.Activity;
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class DroidObserverActivity extends Activity {
     /** Called when the activity is first created. */
@@ -12,6 +19,29 @@ public class DroidObserverActivity extends Activity {
 	       /* TextView tv = new TextView(this);
 	        tv.setText("Lubie placki z serem");
 	        setContentView(tv);*/
+	        LocationManager locMgr= (LocationManager)this.getSystemService(Context.LOCATION_SERVICE);
+			LocationListener locListener = new LocationListener()
+			{
+				
+				public void onLocationChanged(Location location)
+				{
+					if(location!=null)
+					{
+						Toast.makeText(getBaseContext(), "Nowa lokalizacja: szerokœæ [" + location.getLatitude()+"] d³ugoœæ [" +location.getLongitude()+"]",Toast.LENGTH_SHORT).show();
+					}
+				}
+				
+				public void onProviderDisabled(String provider){
+					
+				}
+				public void onProviderEnabled(String provider){
+					
+				}
+				public void onStatusChanged(String provider,int status, Bundle extras){
+					
+				}
+			};
+	        locMgr.requestLocationUpdates(LocationManager.NETWORK_PROVIDER,0,0,locListener);
 	        setContentView(R.layout.main);       
 	    }
 		
@@ -19,8 +49,6 @@ public class DroidObserverActivity extends Activity {
 			TextView tv = new TextView(this);
 	        tv.setText("Lubie placki z serem i miodem");
 	        setContentView(tv);
-			//LocationManager locMgr= (LocationManager)this.getSystemService(Context.LOCATION_SERVICE);
-			//Location loc = locMgr.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-			//List<String> providerList = locMgr.getAllProviders();
+			
 		}
 }
