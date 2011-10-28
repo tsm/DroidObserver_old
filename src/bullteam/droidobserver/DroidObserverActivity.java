5120bae8b10ae9fd74775c06b400187b17c0eeb4
tsm <tomszom@gmail.com>
2011-10-28 21:10:27 +0200

    gpslocator

--- src/bullteam/droidobserver/DroidObserverActivity.java
+++ src/bullteam/droidobserver/DroidObserverActivity.java
@@ -2,12 +2,25 @@ package bullteam.droidobserver;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class DroidObserverActivity extends Activity {
     /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-    }
+	 public void onCreate(Bundle savedInstanceState) {
+	        super.onCreate(savedInstanceState);
+	       /* TextView tv = new TextView(this);
+	        tv.setText("Lubie placki z serem");
+	        setContentView(tv);*/
+	        setContentView(R.layout.main);       
+	    }
+		
+		public void getGPSLocation(View target){
+			TextView tv = new TextView(this);
+	        tv.setText("Lubie placki z serem i miodem");
+	        setContentView(tv);
+			//LocationManager locMgr= (LocationManager)this.getSystemService(Context.LOCATION_SERVICE);
+			//Location loc = locMgr.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+			//List<String> providerList = locMgr.getAllProviders();
+		}
 }
